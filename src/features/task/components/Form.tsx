81db2cb834b8e30d4ec96d61fe994acf9884c4ab
sh-misaki <s.misaki.0000@gmail.com>
2023-06-11 13:10:32 +0900

    warning解消

--- src/features/task/components/Form.tsx
+++ src/features/task/components/Form.tsx
@@ -25,6 +25,7 @@ const TaskForm: React.FunctionComponent<Props> = ({
               id="title"
               name="title"
               value={title}
+              onChange={() => {}}
               className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
             />
           </FormInput>
@@ -39,9 +40,9 @@ const TaskForm: React.FunctionComponent<Props> = ({
               name="about"
               rows={5}
               className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-            >
-              {description}
-            </textarea>
+              value={description}
+              onChange={() => {}}
+            />
           </FormInput>
 
           <FormInput label="Date" name="date">
@@ -49,6 +50,7 @@ const TaskForm: React.FunctionComponent<Props> = ({
               id="date"
               name="date"
               value={date}
+              onChange={() => {}}
               type="date"
               className="w-fullbg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
             />
